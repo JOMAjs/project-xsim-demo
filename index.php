f49c7503b63c566b3c762640dc42cc73e8c2789e
JOMAjs <jeisonarca18@gmail.com>
2024-08-01 17:30:43 -0500

    change update

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 require_once "config/ConfigController.php";
 
 /**************************************************
- * The Controller the template and modules 
+ * The Controller the template and modules ghfgh
  **************************************************/
 require_once "controllers/TemplateController.php";
 
